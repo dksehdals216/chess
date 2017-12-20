78722a664f195334961981af4c017ce88f5ec09c
Dongmin <21600397@handong.edu>
2017-12-20 21:51:00 +0900

    installed dependancies and makefile

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,10 +1,24 @@
-
-
-#include <iostream>
-#include "minesweeper.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	Minesweeper game{9};
-	game.play();
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
