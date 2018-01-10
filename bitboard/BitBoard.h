868c338cdbb72047f275811fbd7f62f67cbcc2b2
Dongmin <21600397@handong.edu>
2018-01-11 02:10:51 +0900

    messing around with bitboards

---
+++ bitboard/BitBoard.h
@@ -0,0 +1,21 @@
+#ifndef BITBOARD_H_
+#define BITBOARD_H_
+
+
+#include <iostream>
+#include <cstdint>  //required for uint64 type
+
+
+typedef uint64_t Bitboard;
+
+class BitBoard
+{
+    public:
+        
+    private:
+        
+};
+
+
+
+#endif //BITBOARD
