6c595902fcfe21b5fe95d582d3d9e9d92afc9c39
Dongmin <21600397@handong.edu>
2018-01-11 01:45:37 +0900

    changed files and finished sfml part into test and main

--- chess/main.cpp
+++ chess/main.cpp
@@ -1,18 +1,12 @@
-
-#include <iostream>
-#include <time.h>
 #include <SFML/Graphics.hpp>
-
-#include "Board.h"
-#include "Player.h"
-#include "Game.h"
-
+#include <time.h>
+#include <string>
+#include <iostream>
 using namespace sf;
 
-int size = 56;
-
-Sprite f[32]; //figures
-
+int defSize = 56;
+int marginSize = 27;
+Sprite f[32]; //figures¬                                                             
 int board[8][8] = 
 {
 		         -1 ,  -2 ,  -3 ,  -4 ,  -5 ,  -3 ,  -2 ,  -1 ,
@@ -22,96 +16,115 @@ int board[8][8] =
 		          0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,
 		          0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,
 		          6 ,   6 ,   6 ,   6 ,   6 ,   6 ,   6 ,   6 ,
-		          1 ,   2 ,   3 ,   4 ,   5 ,   3 ,   2 ,   1 ,
+		          1 ,   2 ,   3 ,   4 ,   5 ,   3 ,   2 ,   1 
 
-};
+};                      
 
-void loadPosition()
+
+std::string toChessNote (Vector2f p)
 {
-	int k = 0;
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			int n = board[i][j];
-			if (!n) continue;
-			int x = abs(n) - 1;
-			int y = n > 0 ? 1:0;
-			f[k].setTextureRect( IntRect(size * x, size * y,size, size));
-			f[k].setPosition(size * j, size * i);
-			k++;
-		}
-	}
+    std::string s = "";
+    s += char(p.x/defSize+97);
+    s += char(7-p.y/defSize+49);
+    return s;
 }
 
-int main ()
+//todo::change this function so that it also includes all other units.
+void loadPosition()
 {
+    int k = 0;
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+        {
+            int n = board[i][j];
+            if (!n) continue;
+            int x = abs(n) - 1;
+            int y = n > 0 ? 1:0;
+            f[k].setTextureRect( IntRect(defSize*x, defSize*y, defSize, defSize));
+            f[k].setPosition(defSize*j + marginSize, defSize*i + marginSize);
+            k++;
+        }
+}
 
-	RenderWindow window(VideoMode(500, 500), "Chess");
 
-	Texture t1, t2;
-	t1.loadFromFile("../images/figures.png", IntRect(0, 0, 15, 15));
-	t2.loadFromFile("../images/board.png");
+int main()
+{
+    int windowSize = 504;
+    RenderWindow window(VideoMode(windowSize, windowSize), "The Chess");
 
+	Texture t1,t2;
+	//t1.loadFromFile("../images/figures.png", IntRect(0, 0, 340/6, 60));
+    t1.loadFromFile("../images/figures.png"); 
+    t2.loadFromFile("../images/board.png");
+
+    Sprite sBoard(t2);
 	Sprite s(t1);
-	Sprite sBoard(t2);
-	
-	for (int i = 0; i < 32; i ++)
-		f[i].setTexture(t1);
+   
+    //sets textures to sprites inside f array, is resized by loadPosition()
+    for(int i = 0; i < 32; i++)
+    {
+        f[i].setTexture(t1);
+    }
 
-	loadPosition();
-	
-	bool isMove = false;
-	float dx = 0, dy = 0;
+    loadPosition();
+
+    bool isMove = false;
+    float dx = 0, dy = 0;
+    Vector2f oldPos, newPos;
+    std::string str;
+    int n = 0;
 
 	while (window.isOpen())
 	{
-		Vector2i pos = Mouse::getPosition(window);
-
-		Event e;
+        Vector2i pos = Mouse::getPosition(window);	
+        Event e;
 		while (window.pollEvent(e))
 		{
 			if (e.type == Event::Closed)
-			{
 				window.close();
-			}
-			
-			//drag and drop	
-			if (e.type == Event::MouseButtonPressed)
-				if (e.key.code == Mouse::Left)
-					if (s.getGlobalBounds().contains(pos.x, pos.y))
-					{
-						isMove = true;
-						dx = pos.x - s.getPosition().x;
-						dy = pos.y - s.getPosition().y;	
-					}
-			
-			if (e.type == Event::MouseButtonReleased)
-			{
-				if (e.key.code == Mouse::Left)
-					{
-						isMove = false;	
-					}
-			}		
-		}
-		
-		if (isMove)
-		{
-			s.setPosition(pos.x-dx, pos.y - dy);
-		}
-		
-		//draw
-		window.clear();
+            
+            //drag and drop
+            if (e.type == Event::MouseButtonPressed)
+                if (e.key.code == Mouse::Left)
+                    for(int i = 0; i < 32; i++)
+                    {
+
+                        if (f[i].getGlobalBounds().contains(pos.x, pos.y))
+                        {
+                            isMove = true;
+                            n = i;
+                            dx = pos.x - f[i].getPosition().x;
+                            dy = pos.y - f[i].getPosition().y;
+                            oldPos = f[i].getPosition();
+                        }
+                    }
+            if (e.type == Event::MouseButtonReleased)
+                if (e.key.code == Mouse::Left)
+                    {
+                        isMove = false;
+                        Vector2f p = f[n].getPosition();// + Vector2f(defSize, defSize);
+                        Vector2f newPos = Vector2f( defSize * int(p.x/defSize) + 25, defSize * int(p.y/defSize) + 25);
+                        str = toChessNote(oldPos) + toChessNote(newPos);
+                        std::cout << str << std::endl;
+                        f[n].setPosition(newPos);
+                    }
+        }
+        
+        if (isMove)
+        {
+            f[n].setPosition(pos.x - dx, pos.y - dy);
+	    }
+
+        window.clear();
 		window.draw(sBoard);
-		window.draw(s);	
-		for (int i = 0; i < 32; i ++)
-		{
-			window.draw(f[i]);	
-		}	
-		window.display();
-	
+	    for(int i = 0; i < 32; i++)
+        {
+            window.draw(f[i]);
+        }
+        
+        window.display();
 	}
 
+	return 0;
 
-	return 0;	
 }
--- chess/test.cpp
+++ chess/test.cpp
@@ -1,28 +1,128 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <string>
+#include <iostream>
 using namespace sf;
+
+int defSize = 56;
+int marginSize = 27;
+Sprite f[32]; //figures¬                                                             
+int board[8][8] = 
+{
+		         -1 ,  -2 ,  -3 ,  -4 ,  -5 ,  -3 ,  -2 ,  -1 ,
+		         -6 ,  -6 ,  -6 ,  -6 ,  -6 ,  -6 ,  -6 ,  -6 ,
+		          0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,
+		          0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,
+		          0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,
+		          0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,   0 ,
+		          6 ,   6 ,   6 ,   6 ,   6 ,   6 ,   6 ,   6 ,
+		          1 ,   2 ,   3 ,   4 ,   5 ,   3 ,   2 ,   1 
+
+};                      
+
+
+std::string toChessNote (Vector2f p)
+{
+    std::string s = "";
+    s += char(p.x/defSize+97);
+    s += char(7-p.y/defSize+49);
+    return s;
+}
+
+//todo::change this function so that it also includes all other units.
+void loadPosition()
+{
+    int k = 0;
+    for(int i = 0; i < 8; i++)
+        for(int j = 0; j < 8; j++)
+        {
+            int n = board[i][j];
+            if (!n) continue;
+            int x = abs(n) - 1;
+            int y = n > 0 ? 1:0;
+            f[k].setTextureRect( IntRect(defSize*x, defSize*y, defSize, defSize));
+            f[k].setPosition(defSize*j + marginSize, defSize*i + marginSize);
+            k++;
+        }
+}
+
 
 int main()
 {
-	RenderWindow window(VideoMode(453, 453), "The Chess");
+    int windowSize = 504;
+    RenderWindow window(VideoMode(windowSize, windowSize), "The Chess");
 
-	Texture t1;
-	t1.loadFromFile("figures.png", IntRect(0, 0, 340/6, 60));
+	Texture t1,t2;
+	//t1.loadFromFile("../images/figures.png", IntRect(0, 0, 340/6, 60));
+    t1.loadFromFile("../images/figures.png"); 
+    t2.loadFromFile("../images/board.png");
 
+    Sprite sBoard(t2);
 	Sprite s(t1);
+   
+    //sets textures to sprites inside f array, is resized by loadPosition()
+    for(int i = 0; i < 32; i++)
+    {
+        f[i].setTexture(t1);
+    }
+
+    loadPosition();
+
+    bool isMove = false;
+    float dx = 0, dy = 0;
+    Vector2f oldPos, newPos;
+    std::string str;
+    int n = 0;
 
 	while (window.isOpen())
 	{
-		Event e;
+        Vector2i pos = Mouse::getPosition(window);	
+        Event e;
 		while (window.pollEvent(e))
 		{
 			if (e.type == Event::Closed)
 				window.close();
-		}
-	
-		window.clear();
-		window.draw(s);
-		window.display();
+            
+            //drag and drop
+            if (e.type == Event::MouseButtonPressed)
+                if (e.key.code == Mouse::Left)
+                    for(int i = 0; i < 32; i++)
+                    {
+
+                        if (f[i].getGlobalBounds().contains(pos.x, pos.y))
+                        {
+                            isMove = true;
+                            n = i;
+                            dx = pos.x - f[i].getPosition().x;
+                            dy = pos.y - f[i].getPosition().y;
+                            oldPos = f[i].getPosition();
+                        }
+                    }
+            if (e.type == Event::MouseButtonReleased)
+                if (e.key.code == Mouse::Left)
+                    {
+                        isMove = false;
+                        Vector2f p = f[n].getPosition();// + Vector2f(defSize, defSize);
+                        Vector2f newPos = Vector2f( defSize * int(p.x/defSize) + 25, defSize * int(p.y/defSize) + 25);
+                        str = toChessNote(oldPos) + toChessNote(newPos);
+                        std::cout << str << std::endl;
+                        f[n].setPosition(newPos);
+                    }
+        }
+        
+        if (isMove)
+        {
+            f[n].setPosition(pos.x - dx, pos.y - dy);
+	    }
+
+        window.clear();
+		window.draw(sBoard);
+	    for(int i = 0; i < 32; i++)
+        {
+            window.draw(f[i]);
+        }
+        
+        window.display();
 	}
 
 	return 0;
